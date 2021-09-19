bcbc8b645fcfd944d4c19b4d480fcfc8b5e2c6c4
suminjung <suminjung@boomco.org>
2021-09-20 00:14:16 +0900

    feat: start uber project

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const Stack = createStackNavigator();
+
+	return (
+		<Provider store={store}>
+				<NavigationContainer>
+					<SafeAreaProvider>
+						<Stack.Navigator>
+							<Stack.Screen 
+								name="HomeScreen"
+								component={HomeScreen}
+								options={{headerShown: false, }}
+							/>
+							<Stack.Screen 
+								name="MapScreen"
+								component={MapScreen}
+								options={{headerShown: false, }}
+							/>
+						</Stack.Navigator>
+					</SafeAreaProvider>
+				</NavigationContainer>
+		</Provider>
+	);
 }
 
 const styles = StyleSheet.create({
